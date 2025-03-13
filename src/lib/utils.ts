bac4a02cb6f0d5f212096094baa31bfe6bf9e315
Nirmal Joji <nirmalsharon.joji@gmail.com>
2025-03-12 23:35:09 -0400

    corrected the URLs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,17 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { logger } from "./logger"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 export function getApiUrl() {
-  return process.env.DEPLOY_ENV === "production"
-    ? process.env.BACKEND_PUBLIC_PRODUCTION_API_URL
-    : process.env.BACKEND_LOCAL_URL
+  const apiUrl = process.env.NODE_ENV === "production" 
+    ? process.env.NEXT_PUBLIC_BACKEND_PRODUCTION_URL || 'https://comment-sense-qkru.onrender.com'
+    : process.env.NEXT_PUBLIC_BACKEND_LOCAL_URL || 'http://localhost:8000'
+    
+
+  
+  return apiUrl
 }
