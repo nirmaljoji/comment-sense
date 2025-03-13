1237999f5a983d664c3d64549c853825b31756bc
Nirmal Joji <nirmalsharon.joji@gmail.com>
2025-03-12 23:23:00 -0400

    url change

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,6 +7,6 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getApiUrl() {
   return process.env.DEPLOY_ENV === "production"
-    ? process.env.NEXT_PUBLIC_PRODUCTION_API_URL
-    : process.env.NEXT_PUBLIC_API_URL
+    ? process.env.BACKEND_PUBLIC_PRODUCTION_API_URL
+    : process.env.BACKEND_LOCAL_URL
 }
